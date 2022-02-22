491c8e93e432c35d03573e5e88214a6f80838602
GTrunSec <gtrunsec@hardenedlinux.org>
2022-02-22 00:40:44 -0800

    debugging

---
+++ .nomake/flake.nix
@@ -0,0 +1,13 @@
+{
+  description =
+    "THIS IS AN AUTO-GENERATED FILE. PLEASE DON'T EDIT IT MANUALLY.";
+  inputs = {
+    nickel-mode = {
+      flake = false;
+      owner = "gtrunsec";
+      repo = "nickel-mode";
+      type = "github";
+    };
+  };
+  outputs = { ... }: { };
+}
